baecd70fade406656f18c63fd30095daae3cd1b2
seba1107 <72539516+seba1107@users.noreply.github.com>
2024-11-30 19:35:59 -0300

    jiji

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import App from './components/app'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import './stylesheets/index.scss'
+import 'bootstrap/dist/css/bootstrap.min.css'; // Estilos de Bootstrap
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Funcionalidades JS de Bootstrap
+
+
+
+document.body.innerHTML = '<div id="root"></div>';
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
--- frontend/src/old_components/Login.js
+++ frontend/src/old_components/Login.js
@@ -28,5 +28,3 @@ const Login = ({ setToken }) => {
     </div>
   );
 };
-
-export default Login;
